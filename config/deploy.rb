933a764fd4e854d26b68d1b2965dea4d342be09e
brad@smithwebtek.com <brad@smithwebtek.com>
2024-10-24 18:05:39 -0400

    Recapify

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,8 +5,8 @@ set :application, "pool-service"
 set :repo_url, "git@github.com:smithwebtek/pool-service.git"
 
 # Default branch is :master
-set :branch, "master"
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deploy/pool-service"
@@ -19,13 +19,13 @@ set :deploy_to, "/home/deploy/pool-service"
 # set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
-set :pty, true
+# set :pty, true
 
 # Default value for :linked_files is []
 append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
